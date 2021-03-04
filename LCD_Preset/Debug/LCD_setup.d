ecca367c4ff2d6923da7c0196a82212dc01fcb02
Pierce Foster <fosterpi@mail.gvsu.edu>
2021-03-04 12:44:33 -0500

    start of LCD

---
+++ LCD_Preset/Debug/LCD_setup.d
@@ -0,0 +1,39 @@
+# FIXED
+
+LCD_setup.obj: ../LCD_setup.c
+LCD_setup.obj: ../LCD_preset.h
+LCD_setup.obj: E:/ccs/ccs_base/arm/include/msp.h
+LCD_setup.obj: E:/ccs/ccs_base/arm/include/msp432p401r.h
+LCD_setup.obj: E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h
+LCD_setup.obj: E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h
+LCD_setup.obj: E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h
+LCD_setup.obj: E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h
+LCD_setup.obj: E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h
+LCD_setup.obj: E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h
+LCD_setup.obj: E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h
+LCD_setup.obj: E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h
+LCD_setup.obj: E:/ccs/ccs_base/arm/include/msp_compatibility.h
+LCD_setup.obj: E:/ccs/ccs_base/arm/include/msp432p401r_classic.h
+LCD_setup.obj: E:/ccs/ccs_base/arm/include/CMSIS/core_cm4.h
+LCD_setup.obj: E:/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+LCD_setup.obj: E:/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+LCD_setup.obj: E:/ccs/ccs_base/arm/include/system_msp432p401r.h
+
+../LCD_setup.c: 
+../LCD_preset.h: 
+E:/ccs/ccs_base/arm/include/msp.h: 
+E:/ccs/ccs_base/arm/include/msp432p401r.h: 
+E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h: 
+E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h: 
+E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h: 
+E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h: 
+E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h: 
+E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h: 
+E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h: 
+E:/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h: 
+E:/ccs/ccs_base/arm/include/msp_compatibility.h: 
+E:/ccs/ccs_base/arm/include/msp432p401r_classic.h: 
+E:/ccs/ccs_base/arm/include/CMSIS/core_cm4.h: 
+E:/ccs/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+E:/ccs/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+E:/ccs/ccs_base/arm/include/system_msp432p401r.h: 
