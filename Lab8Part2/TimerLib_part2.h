6ec91459105ba054670d3a7bced0ab10e44c3fbe
Foster-Pierce <pie.foster199@gmail.com>
2021-03-17 23:45:41 -0400

    Lab 8 parts 1 and 2, not working properly

---
+++ Lab8Part2/TimerLib_part2.h
@@ -0,0 +1,5 @@
+#define LCD_setup_H_INCLUDED
+
+void Setup_Init(void);
+void MotorSpeed(void);
+
--- Lab8part1/DCmotor_setup_part1.c
+++ Lab8part1/DCmotor_setup_part1.c
@@ -3,33 +3,31 @@
 
 void Setup_Init(void){
 
-        //P2.6 init
-        P2SEL1 = 0x00;
-        P2SEL0 = 0x00;
-        P2DIR |= BIT6;
-        P2REN &= ~BIT6;
-        P2OUT &= ~BIT6;
+        //P1.6 init
+        P1SEL1 = 0x00;
+        P1SEL0 = 0x00;
+        P1DIR |= BIT6;
+  //      P3REN &= ~BIT6;
+    //    P3OUT &= ~BIT6;
 }
-void Systick_us_delay(int n){
-    SysTick->LOAD=(n*3-1);
-    SysTick->VAL=0;
-    SysTick->CTRL=0x5;
-    while((SysTick->CTRL & BIT(16))==0);
+void Systick_ms_delay(int n){
     SysTick->CTRL=0;
+    SysTick->LOAD=(n*3000-1);
+    SysTick->VAL=0;
+    SysTick->CTRL=0x05;
+    while((SysTick->CTRL & BIT(16))==0);
 }
 void Systick_DutyCycle(double n){
-    int p = 25000, Ton, Toff;
+    double p = 25, Ton, Toff;
     //n = on/off, run at 40Hz by default
     Ton = n*p;
     Toff = p-Ton;
-    Systick_us_delay(Toff);
+    Systick_ms_delay(Ton);
+    P1OUT &= ~BIT6;
+    Systick_ms_delay(Toff);
 }
 void Systick_MotorSpeed(void){
-    //turn on, wait, turn off, wait, repeat
-    int x=0;
-    P2OUT |= BIT6;
-    Systick_DutyCycle(x);
-    P2OUT &= ~BIT6;
-    Systick_DutyCycle(x);
-
+    double x=1;
+    P1OUT |= BIT6;
+    Systick_DutyCycle(x);       //enter number between 0 and 1
 }
