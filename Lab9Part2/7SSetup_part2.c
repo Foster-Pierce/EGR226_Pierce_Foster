25b57de895afeb2329888ee7b87963a764fb4abc
Foster-Pierce <pie.foster199@gmail.com>
2021-03-24 21:18:21 -0400

    Lab 9 Completed with comments

--- Lab9Part2/7SSetup_part2.c
+++ Lab9Part2/7SSetup_part2.c
@@ -3,7 +3,7 @@
 volatile int n=0;
 
 void Setup_Init(void){
-
+    // initialize the seven segment display
     P2SEL0 =0;
     P2SEL1 =0;
     P2DIR |= (BIT4 | BIT5 | BIT6 | BIT7);
@@ -15,12 +15,13 @@ void Setup_Init(void){
 
 void SysTick_Init_interrupt(void){
     SysTick->CTRL=0;
-    SysTick->LOAD=(1500000-1);
+    SysTick->LOAD=(1500000-1);      //half-second delay between displaying numbers
     SysTick->VAL=0;
     SysTick->CTRL=0x07;
 }
 
 void SysTick_Handler (void) {
+    //updates displayed number then increments
     CaseManager(n);
     if(n!=9)
         n++;
@@ -29,7 +30,7 @@ void SysTick_Handler (void) {
 }
 
 void CaseManager (int n){
-
+    // cases for each possible displayed number
     switch(n) {
     case 0 :
         P2OUT=0;
--- Lab9Part2/TimerLib_part2.h
+++ Lab9Part2/TimerLib_part2.h
@@ -1,8 +1,32 @@
 #ifndef TimerLib_H_INCLUDED
 #define TimerLib_H_INCLUDED
-
+/****| Setup_Init | *****************************************
+ * Brief: Initializes the seven segment display
+ * param: void
+ * return: void
+ *************************************************************/
 void Setup_Init(void);
+
+/****| SysTick_Handler | *****************************************
+ * Brief: SysTick ISR, that updates the displayed number
+ * param: void
+ * return: void
+ *************************************************************/
 void SysTick_Handler (void);
+
+/****| SysTick_Init_interrupt | *****************************************
+ * Brief: Initializes SysTick to delay for a half second then interrupt
+ * param: void
+ * return: void
+ *************************************************************/
 void SysTick_Init_interrupt(void);
+
+/****| CaseManager | *****************************************
+ * Brief: Determines which number to display via the parameter
+ * then selects the correct case to turn on that number
+ * param: int; n, the number that is to be displayed
+ * return: void
+ *************************************************************/
 void CaseManager (int n);
+
 #endif
